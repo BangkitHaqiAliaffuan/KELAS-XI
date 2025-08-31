b9f6d851670f380e2c6017753594e1004c2689b2
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2025-08-31 19:11:09 +0700

    Creating UI/UX

---
+++ LKS 2025/chatgpt-clone/src/assets/assets.js
@@ -0,0 +1,15 @@
+import write from './write.png'
+import light_bulb from './light_bulb.png'
+import pin from './pin.png'
+import voice from './voice.png'
+import globe from './globe.png'
+import question from './question.png'
+
+export const assets = {
+    write,
+    globe,
+    light_bulb,
+    question,
+    pin,
+    voice
+}
