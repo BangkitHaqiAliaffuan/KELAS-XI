2cb0ed4a9ca899cb0ff61fe420b3c5dda112eb34
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2025-09-04 15:18:35 +0700

    Managing response function

--- LKS 2025/chatgpt-clone/src/Chat/Chat.jsx
+++ LKS 2025/chatgpt-clone/src/Chat/Chat.jsx
@@ -1,40 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import "./Chat.css";
+import { fetchGemini } from "../api/gemini";
+// import { send } from "vite";
 const Chat = () => {
+  const [messages, setMessages] = useState([
+    { role: "user", text: "" },
+    { role: "bot", text: "" },
+  ]);
+
+  const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isSend, setIsSend] = useState(false);
+
+  const handleinput = (input) => {
+    input != "" ? setIsSend(true) : setIsSend(false);
+  };
+
+  const handleSend = async () => {
+    if (loading) return;
+    setMessages([...messages, { role: "user", text: input }]);
+    setLoading(true);
+    const reply = await fetchGemini(input);
+    setMessages((msgs) => [...msgs, { role: "bot", text: reply }]);
+    setInput("");
+    setLoading(false);
+  };
+
   return (
     <div className="chat-container">
       <div className="chats">
-        <div className="chat">
-          <img src={assets.user_icon} />
-          <div className="chat-result">
-            <p>
-              Halo! Bisakah Anda membantu saya memahami konsep machine learning
-              dasar?
-            </p>
+        {messages.map((item, index) => (
+          
+          item.text?
+          <div className={`chat${item.role === "bot"?" bot":''}`} key={index}>
+            <img src={item.role=="bot"?assets.logo:assets.chatgpt} />
+            <div className="chat-result">
+              <p>
+                {item.text}
+              </p>
+            </div>
+          </div>:
+          ''
+
+        ))}
+        {loading && (
+          <div className="chat bot">
+            <img src={assets.logo}/>
+            <div className="chat-result">
+              <p>Loading....</p>
+            </div>
           </div>
-        </div>
-        <div className="chat bot">
-          <img src={assets.logo} />
-          <div className="chat-result">
-            <p>
-              Tentu! Machine learning adalah cabang dari artificial intelligence
-              yang memungkinkan komputer untuk belajar dan membuat keputusan
-              dari data tanpa diprogram secara eksplisit. Ada tiga jenis utama:
-              supervised learning (pembelajaran terawasi), unsupervised learning
-              (pembelajaran tidak terawasi), dan reinforcement learning
-              (pembelajaran penguatan). Apakah ada aspek tertentu yang ingin
-              Anda pelajari lebih dalam?
-            </p>
-          </div>
-        </div>
+        )}
       </div>
       <div className="chat-footer">
         <div className="input-prompt">
-          <input type="text" placeholder="Send a message" />
-          <button className="send">
-            <img src={assets.send} />
-          </button>
+          <input
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+              handleinput(e.target.value);
+            }}
+            type="text"
+            placeholder="Send a message"
+          />
+          {isSend ? (
+            <button className="send" onClick={handleSend} disabled={loading}>
+              <img src={assets.send} />
+            </button>
+          ) : (
+            ""
+          )}
         </div>
         <div className="footer">Chat Gpt Model OpenAi</div>
       </div>
