78b97ac503b1f6e6e9be098050bf2b563f99fafd
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2025-09-12 04:59:00 +0700

    initializating ev-web

---
+++ LKS 2025/ev-web/src/assets/assets.js
@@ -0,0 +1,25 @@
+
+import arrow_btn from './arrow_btn.png';
+import email from './email.png';
+import image1 from './image1.png';
+import image2 from './image2.png';
+import image3 from './image3.png';
+import password from './password.png';
+import pause_icon from './pause_icon.png';
+import person from './person.png';
+import play_icon from './play_icon.png';
+import video1 from './video1.mp4';
+
+export const assets ={
+	arrow_btn,
+	email,
+	image1,
+	image2,
+	image3,
+	password,
+	pause_icon,
+	person,
+	play_icon,
+	video1
+
+}
