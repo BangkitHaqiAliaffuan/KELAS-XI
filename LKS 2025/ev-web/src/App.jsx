9c3e065e2c06426c6cd09a8161d4c5597a4295d2
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2025-09-13 00:35:45 +0700

    Done

--- LKS 2025/ev-web/src/App.jsx
+++ LKS 2025/ev-web/src/App.jsx
@@ -1,62 +1,112 @@
-import React from 'react'
-import './App.css'
-import {assets} from './assets/assets.js'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { assets } from "./assets/assets.js";
 const App = () => {
-  return (
-    <div style={{backgroundImage:`url(${assets.image1})`}} className='main-container'>
-      <nav className='nav'>
-        <div className='logo'>
-          EV-olution
-        </div>
+  const [text, setText] = useState("Dive into \n what you love");
+  const [bg, setBg] = useState(assets.image1);
+  const [play, setPlay] = useState(false);
+  const [active, setActive] = useState(1);
 
-        <div className='nav-items'>
-          <div className='item'>
-            Home
-          </div>
-          <div className='item'>
-            Explore
-          </div>
-          <div className='item'>
-            About
-          </div>
-          <div className='item active'>
-            Contact
-          </div>
+
+  useEffect(()=>{
+    
+   active == 1 ? handleClick("Dive into \n what you love", assets.image1) : 
+   active == 2 ?  handleClick("Indulge \n your passions", assets.image2):
+   active == 3 ?  handleClick("Indulge \n your passions", assets.image3) : ""
+  })
+
+  
+const useEffectActive = useEffect(()=>{
+    if(!play){
+      const interval = setInterval(()=>{
+        setActive(prev=>prev >= 3 ? 1 : prev+1)
+      }, 3000)
+      return ()=>clearInterval(interval)
+    }
+  }, [play])
+
+  
+  
+
+  const handleClick = (text, url) => {
+    setText(text);
+    setBg(url);
+    // console.log(bg);
+  };
+
+  return (
+    <div style={{ backgroundImage: `url(${bg})` }} className="main-container">
+      {play ? <video src={assets.video1} autoPlay muted loop/>:""}
+      <nav className="nav">
+        <div className="logo">EV-olution</div>
+
+        <div className="nav-items">
+          <div className="item">Home</div>
+          <div className="item">Explore</div>
+          <div className="item">About</div>
+          <div className="item active">Contact</div>
         </div>
       </nav>
 
-      <div className='hero'>
-        <div className='title'>
-          <h1>Give in to your passions</h1>
+      <div className="hero">
+        <div className="title">
+          <p className="title">
+            {text.split("\n").map((line, idx) => (
+              <React.Fragment key={idx}>
+                {line}
+                {idx !== text.split("\n").length - 1 && <br />}
+                {/* {console.log(idx, text.split("\n").length - 1)} */}
+              </React.Fragment>
+            ))}
+          </p>
         </div>
 
-        <div className='btn-wrapper'>
+        <div className="btn-wrapper">
           <p>Explore the features</p>
           <button>
-            <img src={assets.arrow_btn}/>
+            <img src={assets.arrow_btn} />
           </button>
         </div>
       </div>
 
-    <div className='features'>
-      <div className='round-btns'>
-        <div className='btn'></div>
-        <div className='btn'></div>
-        <div className='btn active'></div>
-      </div>
+      <div className="features">
+        <div className="round-btns">
+          <div
+            onClick={() => {
+              handleClick("Dive into \n what you love", assets.image1);
 
-      <div className='play-container'>
-        <div className='btn-play'>
-          <img src={assets.play_icon}/>
+              setActive(1)
+            }}
+            className={`btn ${active == 1 && "active"}`}
+          ></div>
+          
+          <div
+            onClick={() => {
+              handleClick("Indulge \n your passions", assets.image2);
+                            setActive(2)
+            }}
+                        className={`btn ${active === 2 && "active"}`}
+          ></div>
+          
+          <div
+            onClick={() => {
+              handleClick("Give in to \n your passions", assets.image3);
+                            setActive(3)
+            }}
+                        className={`btn ${active === 3 && "active"}`}
+          ></div>
+          
         </div>
 
-        See the video
+        <div className="play-container">
+          <div onClick={()=>{ setPlay(prev=>!prev)}} className="btn-play">
+            <img src={!play? assets.play_icon:assets.pause_icon} />
+          </div>
+          See the video
+        </div>
       </div>
     </div>
+  );
+};
 
-
-    </div>
-  )
-}
-
-export default App
+export default App;
