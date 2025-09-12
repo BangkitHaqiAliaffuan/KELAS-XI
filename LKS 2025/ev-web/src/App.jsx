6f0f40717fda037fc3cdd3195e2551fd68bb432c
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2025-09-12 09:10:10 +0700

    Creating UI/UX

--- LKS 2025/ev-web/src/App.jsx
+++ LKS 2025/ev-web/src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import {assets} from './assets/assets.js'
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{backgroundImage:`url(${assets.image1})`}} className='main-container'>
+      <nav className='nav'>
+        <div className='logo'>
+          EV-olution
+        </div>
+
+        <div className='nav-items'>
+          <div className='item'>
+            Home
+          </div>
+          <div className='item'>
+            Explore
+          </div>
+          <div className='item'>
+            About
+          </div>
+          <div className='item active'>
+            Contact
+          </div>
+        </div>
+      </nav>
+
+      <div className='hero'>
+        <div className='title'>
+          <h1>Give in to your passions</h1>
+        </div>
+
+        <div className='btn-wrapper'>
+          <p>Explore the features</p>
+          <button>
+            <img src={assets.arrow_btn}/>
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+    <div className='features'>
+      <div className='round-btns'>
+        <div className='btn'></div>
+        <div className='btn'></div>
+        <div className='btn active'></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className='play-container'>
+        <div className='btn-play'>
+          <img src={assets.play_icon}/>
+        </div>
+
+        See the video
+      </div>
+    </div>
+
+
+    </div>
   )
 }
 
