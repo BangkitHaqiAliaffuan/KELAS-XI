e68ee9702a26713b178682172ce347225ac56999
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2025-09-22 04:10:01 +0700

    Portofolio Section

--- LKS 2025/portofolio/src/Components/About/About.jsx
+++ LKS 2025/portofolio/src/Components/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section id='skills'>
       <span className='skill-title'>What I do </span>
-      <span className='skill-description'>i'm a skilled and passionate Web Developer with 2 years of experience</span>
+  <span className='skill-description'>Saya seorang Web Developer yang berpengalaman selama 2 tahun, fokus pada pembuatan situs dan aplikasi yang responsif serta mudah digunakan. Saya mengutamakan kualitas kode, pengalaman pengguna, dan solusi yang dapat diskalakan.</span>
       <div className='skill-bars'>
         <div className='skill-bar'>
           <img src={assets.uiDesign}/>
@@ -14,20 +14,23 @@ const About = () => {
             <p>this is a demo text, you can write your own content here</p>
           </div>
         </div>
+        
         <div className='skill-bar'>
           <img src={assets.websiteDesign}/>
           <div className='skill-bar-text'>
-            <h2></h2>
-            <p></p>
+            <h2>Web Design</h2>
+            <p>this is a demo text, you can write your own content here</p>
           </div>
         </div>
+        
         <div className='skill-bar'>
           <img src={assets.appDesign}/>
           <div className='skill-bar-text'>
-            <h2></h2>
-            <p></p>
+            <h2>App Design</h2>
+            <p>this is a demo text, you can write your own content here</p>
           </div>
         </div>
+        
       </div>
     </section>
   )
