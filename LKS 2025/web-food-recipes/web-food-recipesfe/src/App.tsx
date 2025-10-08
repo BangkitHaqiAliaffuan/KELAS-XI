9312442b13c0e96b18c42992b7f232f240908136
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2025-10-08 07:19:35 +0700

    Creating Recipe Detail Page

--- LKS 2025/web-food-recipes/web-food-recipesfe/src/App.tsx
+++ LKS 2025/web-food-recipes/web-food-recipesfe/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import RecipeDetail from './pages/RecipeDetail';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
           <Route path="/categories" element={<div style={{ padding: '2rem' }}>Categories Page (Coming Soon)</div>} />
           <Route path="/recipes" element={<div style={{ padding: '2rem' }}>Recipes Page (Coming Soon)</div>} />
           <Route path="/categories/:slug" element={<div style={{ padding: '2rem' }}>Category Detail (Coming Soon)</div>} />
-          <Route path="/recipes/:slug" element={<div style={{ padding: '2rem' }}>Recipe Detail (Coming Soon)</div>} />
+          <Route path="/recipes/:slug" element={<RecipeDetail />} />
         </Routes>
       </Layout>
     </Router>
