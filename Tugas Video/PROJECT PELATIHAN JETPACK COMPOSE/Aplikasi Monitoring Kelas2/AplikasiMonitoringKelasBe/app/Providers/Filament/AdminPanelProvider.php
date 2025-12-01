e9d88ed6f2d37114ccf061fea480d7b7008889ad
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2025-12-01 20:05:17 +0700

    Selesai

--- Tugas Video/PROJECT PELATIHAN JETPACK COMPOSE/Aplikasi Monitoring Kelas2/AplikasiMonitoringKelasBe/app/Providers/Filament/AdminPanelProvider.php
+++ Tugas Video/PROJECT PELATIHAN JETPACK COMPOSE/Aplikasi Monitoring Kelas2/AplikasiMonitoringKelasBe/app/Providers/Filament/AdminPanelProvider.php
@@ -122,7 +122,6 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->viteTheme('resources/css/filament/admin/theme.css')
             ->spa();
     }
 }
