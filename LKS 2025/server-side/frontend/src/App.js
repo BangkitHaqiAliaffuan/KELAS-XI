be1454055a4631daed35c9352ca8832a602fd919
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2026-01-21 05:01:05 +0700

    Login Functiopn

--- LKS 2025/server-side/frontend/src/App.js
+++ LKS 2025/server-side/frontend/src/App.js
@@ -1,71 +1,127 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [idnumber, setIdnumber] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {
+        method: "POST",
+         headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          idnumber: idnumber,
+          password: password,
+        }),
+      });
+
+    //   console.log(idnumber)
+
+      localStorage.setItem('token', data.test.login_tokens)
+
+      const data = await response.json();
+    //   console.log(data.test.login_tokens);
+    
+    } catch (error) {}
+  };
+
   return (
     <>
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
-    <div class="container">
-        <a class="navbar-brand" href="#">Job Seekers Platform</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            Job Seekers Platform
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
-        </button>
+          </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Login
+                </a>
+              </li>
             </ul>
+          </div>
         </div>
-    </div>
-</nav>
+      </nav>
 
-<main>
-    
-    <header class="jumbotron">
-        <div class="container text-center m-5">
+      <main>
+        <header class="jumbotron">
+          <div class="container text-center m-5">
             <h1 class="display-4">Job Seekers Platform</h1>
-        </div>
-    </header>
-    
+          </div>
+        </header>
 
-    <div class="container">
-        <div class="row justify-content-center">
+        <div class="container">
+          <div class="row justify-content-center">
             <div class="col-md-6">
-                <form class="card card-default">
-                    <div class="card-header">
-                        <h4 class="mb-0">Login</h4>
+              <form class="card card-default" onSubmit={handleSubmit}>
+                <div class="card-header">
+                  <h4 class="mb-0">Login</h4>
+                </div>
+                <div class="card-body">
+                  <div class="form-group row align-items-center">
+                    <div class="col-4 text-right">ID Card Number</div>
+                    <div class="col-8">
+                      <input
+                        type="text"
+                        class="form-control"
+                        value={idnumber}
+                        onChange={(e) => setIdnumber(e.target.value)}
+                      />
                     </div>
-                    <div class="card-body">
-                        <div class="form-group row align-items-center">
-                            <div class="col-4 text-right">ID Card Number</div>
-                            <div class="col-8"><input type="text" class="form-control"/></div>
-                        </div>
-                        <div class="form-group row align-items-center">
-                            <div class="col-4 text-right">Password</div>
-                            <div class="col-8"><input type="password" class="form-control"/></div>
-                        </div>
-                        <div class="form-group row align-items-center mt-4">
-                            <div class="col-4"></div>
-                            <div class="col-8"><button class="btn btn-primary">Login</button></div>
-                        </div>
+                  </div>
+                  <div class="form-group row align-items-center">
+                    <div class="col-4 text-right">Password</div>
+                    <div class="col-8">
+                      <input
+                        type="password"
+                        class="form-control"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
                     </div>
-                </form>
+                  </div>
+                  <div class="form-group row align-items-center mt-4">
+                    <div class="col-4"></div>
+                    <div class="col-8">
+                      <button class="btn btn-primary">Login</button>
+                    </div>
+                  </div>
+                </div>
+              </form>
             </div>
+          </div>
         </div>
-    </div>
-</main>
+      </main>
 
-
-<footer>
-    <div class="container">
-        <div class="text-center py-4 text-muted">
+      <footer>
+        <div class="container">
+          <div class="text-center py-4 text-muted">
             Copyright &copy; 2023 - Web Tech ID
+          </div>
         </div>
-    </div>
-</footer>
-</>
-  )
-}
+      </footer>
+    </>
+  );
+};
 
-export default App
+export default App;
